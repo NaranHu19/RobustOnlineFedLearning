bdeb0f817654423a597991f80d4d2b890dd6ecf3
Lilian Say <say@lpsm.paris>
2026-08-21 16:03:36 +0200

    [FIX] Correct path to weight_decay

--- main.py
+++ main.py
@@ -266,7 +266,7 @@ def eliminate_experiments_done(dict_list):
             f"{'_'.join(pre_aggregation_names)}_"
             f"{setting['attack']['name']}_"
             f"lr_{setting['model']['learning_rate']}_"
-            f"wd_{setting['honest_clients']['weight_decay']}"
+            f"wd_{setting['model']['weight_decay']}"
         )
 
         if folder_name in folders:
